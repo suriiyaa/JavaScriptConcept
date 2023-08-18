3fc4161bd263364ca7e0d6a14ed4be72d61d6c46
suriiyaa <103480720+suriiyaa@users.noreply.github.com>
2023-08-18 15:24:03 +0000

    New file Added

---
+++ OperationJavaScript/stackAndHeap.js
@@ -0,0 +1,21 @@
+let name = "surya"
+console.log(name);
+
+let name1= name ;
+
+console.log(name1);
+
+const userOne = 
+{
+    name : "surya",
+    email: "surya@example.com",
+    password: "12345"
+}
+
+console.log(userOne);
+let userTwo = userOne ;
+
+userTwo = "surya@demo.com"
+
+console.log(userTwo);
+console.log(userOne);
---
+++ basic/string.js
@@ -0,0 +1,14 @@
+//learned string and their methods
+let firstName = "surya"
+
+let city = "odisha"
+// convert from lower to upper case
+console.log(`Hello my name is ${firstName.toUpperCase()} and i belongs to ${city.toUpperCase()}`);
+
+const fname = new String("surya")
+
+console.log(fname.length);
+console.log(fname.toUpperCase());
+console.log(fname.toLowerCase());
+console.log(fname.charAt(2)); //index starts from 0
+console.log(fname.indexOf('r'));
